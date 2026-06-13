18497ee5f62b603d6131c1ad67d9be2a5453eb1e
A. Dimas Cahyaning Furqon <cahyaningdimas@gmail.com>
2026-06-13 11:31:17 +1000

    specify the model version (v5.20) to make sure the result is reproducible

--- scripts/basecall.sh
+++ scripts/basecall.sh
@@ -7,6 +7,29 @@ model=$2
 outdir=$3
 trim_flag=${4:-""} # Default is empty (trimming enabled)
 
+case "$model" in
+    "hac")
+        model_version="dna_r10.4.1_e8.2_400bps_hac@v5.2.0"
+        ;;
+    "sup")
+        model_version="dna_r10.4.1_e8.2_400bps_sup@v5.2.0"
+        ;;
+    *)
+		echo "Error: Model must be 'hac' or 'sup'. You provided '$model'." >&2
+        exit 1
+        ;;
+esac
+
+# Add log so that the paramters are captured
+echo "================================================================="
+echo "DORADO BASECALLING START"
+echo "Input Directory : $dir"
+echo "Explicit Model  : $model_version"
+echo "Output Directory: $outdir"
+echo "Trim Flag       : ${trim_flag:-"Enabled (Default)"}"
+echo "================================================================="
+
+
 mkdir -p "$outdir"
 
 for run in "$dir"/*/; do
@@ -28,6 +51,6 @@ for run in "$dir"/*/; do
         esac
     fi
 
-    dorado basecaller $trim_flag $kit_flag --recursive "$model" "$run" > "$outdir/$name.bam"
+    dorado basecaller $trim_flag $kit_flag --recursive "$model_version" "$run" > "$outdir/$name.bam"
 done
 # ssubmit -t 1d -m 16g basecall "./scripts/basecall.sh <inputdir> <model>" -- --qos gpu -p gpu_cuda --gres=gpu:h100:1
